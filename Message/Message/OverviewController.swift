48573c66e622fe00a74ffd09b28e78f895fbc10b
Finn Kiesinger <fkiesinger@me.com>
2019-03-10 12:29:01 +0100

    setup app without storyboard

---
+++ Message/Message/OverviewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Message
+//
+//  Created by Finn Kiesinger on 10.03.19.
+//  Copyright © 2019 devCore. All rights reserved.
+//
+
+import UIKit
+
+class OverviewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        setupView()
+        
+    }
+    
+//    func setupView() {
+//        navigationItem.title = "Chats"
+//        navigationController?.navigationBar.prefersLargeTitles = true
+//    }
+    
+}
+
